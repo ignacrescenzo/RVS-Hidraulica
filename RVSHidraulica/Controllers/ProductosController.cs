14a72106e756c2337c4eb2055b757fbe57610fe7
Ignacio Crescenzo <ignadario97@gmail.com>
2019-12-08 11:32:16 -0300

    Avances

---
+++ RVSHidraulica/Controllers/ProductosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RVSHidraulica.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult BombasYMotores()
+        {
+            return View();
+        }
+
+        public IActionResult ManguerasYAccesorios()
+        {
+            return View();
+        }
+
+        public IActionResult Cilindros()
+        {
+            return View();
+        }
+    }
+}
